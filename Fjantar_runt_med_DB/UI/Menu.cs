8dcd535750077a30dfe443c7224bbc7ecaf5778b
Niklas H <156240330+mymh13@users.noreply.github.com>
2024-11-12 09:43:57 +0100

    added a while-loop for the menu

--- Fjantar_runt_med_DB/UI/Menu.cs
+++ Fjantar_runt_med_DB/UI/Menu.cs
@@ -6,50 +6,59 @@ using System.Threading.Tasks;
 
 namespace Fjantar_runt_med_DB.UI
 {
+    using Fjantar_runt_med_DB.DatabaseConnections;
+
     internal class Menu
     {
         internal static void MenuChoices()
         {
-            Console.WriteLine("Choose a database type:");
-            Console.WriteLine("1. MySQL");
-            Console.WriteLine("2. SQLite");
-            Console.WriteLine("3. MongoDB");
-            Console.WriteLine("4. API");
-            Console.WriteLine("5. Exit\n");
+            DatabaseType dbType = default;
+            bool validChoice = false;
 
-            Console.WriteLine("Enter your choice:");
-
-            string? menuInput = Console.ReadLine();
-            DatabaseType dbType;
-
-            switch (menuInput)
+            while (!validChoice)
             {
-                case "1":
-                    dbType = DatabaseType.MySQL;
-                    Console.WriteLine("You chose MySQL.");
-                    break;
-                case "2":
-                    dbType = DatabaseType.SQLite;
-                    Console.WriteLine("You chose SQLite.");
-                    break;
-                case "3":
-                    dbType = DatabaseType.MongoDB;
-                    Console.WriteLine("You chose MongoDB.");
-                    break;
-                case "4":
-                    dbType = DatabaseType.API;
-                    Console.WriteLine("You chose API yada yada-something");
-                    break;
-                case "5":
-                    Program.ApplicationShutdown();
-                    break;
-                default:
-                    Console.WriteLine("Invalid choice, please try again.");
-                    MenuChoices();
-                    break;
+                Console.WriteLine("Choose a database type:");
+                Console.WriteLine("1. MySQL");
+                Console.WriteLine("2. SQLite");
+                Console.WriteLine("3. MongoDB");
+                Console.WriteLine("4. API");
+                Console.WriteLine("5. Exit\n");
+
+                Console.WriteLine("Enter your choice:");
+                string? menuInput = Console.ReadLine();
+
+                switch (menuInput)
+                {
+                    case "1":
+                        dbType = DatabaseType.MySQL;
+                        validChoice = true;
+                        Console.WriteLine("You chose MySQL.");
+                        break;
+                    case "2":
+                        dbType = DatabaseType.SQLite;
+                        validChoice = true;
+                        Console.WriteLine("You chose SQLite.");
+                        break;
+                    case "3":
+                        dbType = DatabaseType.MongoDB;
+                        validChoice = true;
+                        Console.WriteLine("You chose MongoDB.");
+                        break;
+                    case "4":
+                        dbType = DatabaseType.API;
+                        validChoice = true;
+                        Console.WriteLine("You chose API.");
+                        break;
+                    case "5":
+                        Program.ApplicationShutdown();
+                        return;
+                    default:
+                        Console.WriteLine("Invalid choice, please try again.");
+                        break;
+                }
             }
 
-            // Create a single instance of DatabaseManager with the selected DatabaseType
+            // Now that we have a valid dbType, we can safely create the DatabaseManager
             var dbManager = new DatabaseManager(dbType);
             Console.WriteLine($"Connected to {dbType}.");
         }
